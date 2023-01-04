e5047994f3c91589cc21c8ef6d9b27b788be7a29
Ruslan Symonenko <symonenkors@gmail.com>
2023-01-04 13:23:39 +0200

    made module for rendering tasks

--- src/js/modules/addNewTasks.js
+++ src/js/modules/addNewTasks.js
@@ -1,10 +1,12 @@
+import {taskRender} from './taskRender.js'
+
 export function addNewTasks () {
   const inputNewTaskText = document.querySelector('.new-task__text');
   const btnAddNewTask = document.querySelector('.new-task__add-btn');
 
   btnAddNewTask.addEventListener('click', () => {
     if (inputNewTaskText.value) {
-      console.log(inputNewTaskText.value);
+      taskRender(inputNewTaskText.value);
     } else {
       alert('Please, write task text');
     }
---
+++ src/js/modules/taskRender.js
@@ -0,0 +1,56 @@
+export function taskRender (taskText, checked = false, favorite = false) {
+  const taskList = document.querySelector('.main-tasks__list');
+
+  let taskForRendering = {
+    check: checked,
+    text: taskText,
+    isFavorite: favorite
+  };
+
+  function createTask (check, text, isFavorite = false) {
+    let task = document.createElement('li');
+    task.classList.add('main-tasks__list-item');
+
+    let checkBox = document.createElement('input');
+    checkBox.classList.add('task-check');
+    checkBox.setAttribute('type', 'checkbox');
+    checkBox.value = check;
+
+    let textContainer = document.createElement('div');
+    textContainer.classList.add('task-text');
+    textContainer.textContent = text;
+
+    let btnsContainer = document.createElement('div');
+    btnsContainer.classList.add('task-btns');
+
+
+
+    let favoriteBtn = document.createElement('button');
+    favoriteBtn.classList.add('task-favorite__btn');
+
+    let favoriteImg = document.createElement('img');
+    favoriteImg.setAttribute('src', './src/images/favorite-ico.svg');
+
+    favoriteBtn.append(favoriteImg);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('task-delete__btn');
+
+    let deleteImg = document.createElement('img');
+    deleteImg.setAttribute('src', './src/images/trash-ico.svg');
+
+    deleteBtn.append(deleteImg);
+
+    btnsContainer.append(favoriteBtn);
+    btnsContainer.append(deleteBtn);
+    
+
+    task.append(checkBox);
+    task.append(textContainer);
+    task.append(btnsContainer);
+
+    taskList.append(task);
+  }
+
+  createTask(taskForRendering.check, taskForRendering.text, taskForRendering.isFavorite);
+}
